d3bb192d1d9a498ade45e5c761dd632d3c1f94ba
Alex Gaesser <agaesser@gmail.com>
2021-09-06 15:30:04 -0700

    wip csv uploader

--- README.md
+++ README.md
@@ -7,3 +7,13 @@
 - users should be able to view a list of all constituents
 - users should be able to create individual constituent record
 - users should be able to update or delete individual constituent record
+
+## Use
+
+Send a test CSV to be uploaded to the `users/add` path from the project root via:
+
+```
+curl -X POST -H 'Content-Type: text/csv' -d @fixtures/records.csv http://localhost:3000/users/add
+```
+
+As submitted, this parses the entire CSV into a single line. Next steps are to instead parse each record into individual lines, perform validation on each field, and store it into a `UserRecord` struct with a generated uuid. The array of structs is used as the imput to the (unimplemented) `InsertRecord` method, which is responsible for inserting user records into the data store.
--- service/service.go
+++ service/service.go
@@ -1,8 +1,10 @@
 package service
 
 import (
+	"encoding/csv"
 	"fmt"
 	"net/http"
+	"strconv"
 )
 
 type ServiceConfig struct{}
@@ -10,6 +12,16 @@ type ServiceConfig struct{}
 // Service implements the service.
 type Service struct{}
 
+// User represents a user record
+// todo: unique UUID
+type User struct {
+	Email     string
+	FirstName string
+	LastName  string
+	ZipCode   int
+}
+
+// NewService instatiates a service with given config optiions
 func NewService(config ServiceConfig) Service {
 	return Service{}
 }
@@ -29,6 +41,7 @@ func ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello %s!\n", r.URL.Path[1:])
 }
 
+// hello handles default or unexpected url paths
 func hello() http.Handler {
 	return http.HandlerFunc(ServeHTTP)
 }
@@ -36,28 +49,28 @@ func hello() http.Handler {
 // AddUserRecord inserts a single user record into the data store.
 func (s *Service) AddUserRecord() http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("AddUserRecord")
+		// todo
 	})
 }
 
 // ListUserRecord fetches a specific user record.
 func (s *Service) ListUserRecord() http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("ListUserRecord")
+		// todo
 	})
 }
 
 // UpdateUserRecord updates a specific user record already present in the data store.
 func (s *Service) UpdateUserRecord() http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("UpdateUserRecord")
+		// todo
 	})
 }
 
 // ListUserRecords lists all user records contained in the data store.
 func (s *Service) ListUserRecords() http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("ListUserRecords")
+		// todo
 	})
 }
 
@@ -65,6 +78,37 @@ func (s *Service) ListUserRecords() http.HandlerFunc {
 // We assume that all CSVs have Email,FirstName,LastName,ZipCode header rows.
 func (s *Service) UploadCSV() http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("UploadCSV")
+		reader := csv.NewReader(r.Body)
+
+		lines, err := reader.ReadAll()
+		if err != nil {
+			panic(err)
+		}
+
+		records := make([]User, 0)
+
+		for i, line := range lines {
+			fmt.Println("line: ", line)
+			// skip header
+			if i == 0 {
+				continue
+			}
+
+			zipCode, err := strconv.Atoi(line[3])
+			if err != nil {
+				panic(err)
+			}
+
+			record := User{
+				Email:     line[0],
+				FirstName: line[1],
+				LastName:  line[2],
+				ZipCode:   zipCode,
+			}
+
+			records = append(records, record)
+		}
+
+		// todo: dbService.InsertRecords(records)
 	})
 }
